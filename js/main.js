584424ac01e520024a8dbde883e196422f6edd63
Logan Brock <logan.g.brock@gmail.com>
2021-06-17 11:03:03 -0400

    Coding Meetup 2 codewars challenge

--- js/main.js
+++ js/main.js
@@ -8,3 +8,10 @@ p=> psuedocode
 // Coding Meetup #1 - Higher-Order Functions Series - Count the number of JavaScript developers coming from Europe
 
 const countDevelopers = (list) => list.filter(x => x.continent === 'Europe' && x.language === 'JavaScript').length
+
+// Coding Meetup #2 - Higher-Order Functions Series - Greet developers
+
+const greetDevelopers = (list) => {
+    list.forEach(x => x.greeting = `Hi ${x.firstName}, what do you like the most about ${x.language}?`)
+    return list
+  }
