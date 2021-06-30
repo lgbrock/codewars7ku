21ad519f9a7ad5a5463aeecc1ab62afc2314fb09
Logan Brock <logan.g.brock@gmail.com>
2021-06-30 09:32:06 -0400

    Coding meetup 9 codewars challenge

--- js/main.js
+++ js/main.js
@@ -57,5 +57,26 @@ const findSenior = (list) => {
 };
 
 // Coding Meetup #8 - Higher-Order Functions Series - Will all continents be represented?
-const allContinents = (list) => ["Africa", "Americas", "Asia", "Europe", "Oceania"].every(x =>
-    list.some(y => x == y.continent))
+const allContinents = (list) =>
+	['Africa', 'Americas', 'Asia', 'Europe', 'Oceania'].every((x) =>
+		list.some((y) => x == y.continent)
+	);
+
+// Coding Meetup #9 - Higher-Order Functions Series - Is the meetup age-diverse?
+const isAgeDiverse = (list) => {
+	const check = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
+	list.map((v) => {
+		const age = v.age;
+		age > 9 && age < 20 ? (check[0] = 1) : 0;
+		age > 19 && age < 30 ? (check[1] = 1) : 0;
+		age > 29 && age < 40 ? (check[2] = 1) : 0;
+		age > 39 && age < 50 ? (check[3] = 1) : 0;
+		age > 49 && age < 60 ? (check[4] = 1) : 0;
+		age > 59 && age < 70 ? (check[5] = 1) : 0;
+		age > 69 && age < 80 ? (check[6] = 1) : 0;
+		age > 79 && age < 90 ? (check[7] = 1) : 0;
+		age > 89 && age < 100 ? (check[8] = 1) : 0;
+		age > 100 ? (check[9] = 1) : 0;
+	});
+	return check.every((v) => v === 1);
+};
