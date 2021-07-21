ccc57daa733513b3bbb81824bf78471236f85d63
Logan Brock <logan.g.brock@gmail.com>
2021-07-21 10:43:37 -0600

    Coding Meetup #17 - Higher-Order Functions Series - Sort by programming language

--- js/main.js
+++ js/main.js
@@ -165,3 +165,13 @@ const askForMissingDetails = (list) => {
 	});
 	return arr;
 };
+
+// Coding Meetup #17 - Higher-Order Functions Series - Sort by programming language
+const sortByLanguage = (list) => {
+	list.sort((a, b) =>
+		a.language === b.language
+			? a.firstName.localeCompare(b.firstName)
+			: a.language.localeCompare(b.language)
+	);
+	return list;
+};
