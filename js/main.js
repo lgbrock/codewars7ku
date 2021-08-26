4b3cfbea9518a7e7d23233cd74031b48141601ff
Logan Brock <logan.g.brock@gmail.com>
2021-08-26 11:11:44 -0400

    Persistent Bugger

--- js/main.js
+++ js/main.js
@@ -263,3 +263,19 @@ const duplicateEncode = (word) =>
 			return arr.indexOf(v) == arr.lastIndexOf(v) ? '(' : ')';
 		})
 		.join('');
+
+// Persistent Bugger
+const persistence = (num) => {
+	let times = 0;
+	num = num.toString();
+
+	while (num.length > 1) {
+		times++;
+		num = num
+			.split(' ')
+			.map(Number)
+			.reduce((a, b) => a * b)
+			.toString();
+	}
+	return times;
+};
