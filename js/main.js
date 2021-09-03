5a963ee8f8ee35c6a69d5c86c6666cb111b2831e
Logan Brock <logan.g.brock@gmail.com>
2021-09-03 10:08:08 -0400

    Split Strings

--- js/main.js
+++ js/main.js
@@ -371,3 +371,13 @@ const list = (names) => {
 		);
 	return names.map((v) => v.name).slice(-1) + '';
 };
+
+// Split Strings
+const solution = (str) => {
+	let arr = [];
+	for (let i = 0; i < str.split('').length; i += 2) {
+		arr.push(str.slice(i, i + 2));
+	}
+
+	return arr.map((v) => (v.length === 1 ? v + '_' : v));
+};
