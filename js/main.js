0d03bd82068693e45a759254e6450be7ee64e2e4
Logan Brock <logan.g.brock@gmail.com>
2021-09-04 09:42:38 -0400

    Human readable duration format

--- js/main.js
+++ js/main.js
@@ -391,3 +391,23 @@ const anagrams = (word, words) => {
 	});
 	return arr;
 };
+
+// Human readable duration format
+const formatDuration = (seconds) => {
+	let time = { year: 31536000, day: 86400, hour: 3600, minute: 60, second: 1 },
+		res = [];
+
+	if (seconds === 0) return 'now';
+
+	for (var key in time) {
+		if (seconds >= time[key]) {
+			var val = Math.floor(seconds / time[key]);
+			res.push((val += val > 1 ? ' ' + key + 's' : ' ' + key));
+			seconds = seconds % time[key];
+		}
+	}
+
+	return res.length > 1
+		? res.join(', ').replace(/,([^,]*)$/, ' and' + '$1')
+		: res[0];
+};
