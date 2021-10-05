e0f265c6b1ea0c3c02719f0645a333c6ba550649
Logan Brock <logan.g.brock@gmail.com>
2021-10-05 09:08:19 -0400

    Jaden Casing Strings

--- js/main.js
+++ js/main.js
@@ -455,3 +455,10 @@ const spinWords = (s) => {
 		})
 		.join(' ');
 };
+
+// Jaden Casing Strings
+String.prototype.toJadenCase = function () {
+	return this.split(' ')
+		.map((v) => v[0].toUpperCase() + v.slice(1))
+		.join(' ');
+};
