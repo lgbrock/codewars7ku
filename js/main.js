98680317a3273bc199edc61945b84898ad732987
Logan Brock <logan.g.brock@gmail.com>
2021-11-21 11:58:09 -0600

    quick update

--- js/main.js
+++ js/main.js
@@ -853,4 +853,4 @@ const sumDigits = (num) =>
 // smallEnough(4, [1, 2, 5, 3]) // true
 const smallEnough = (a, limit) => a.every((v) => v <= limit);
 
-// Quick update before Full Stack Open
+// Quick update before practice later
